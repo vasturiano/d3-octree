0d571d46b2ca40c20b2ee86baddf11324e86736b
Vasco Asturiano <vasturiano@medallia.com>
2017-03-15 15:06:08 -0700

    Fix size-test

--- test/size-test.js
+++ test/size-test.js
@@ -4,14 +4,14 @@ var tape = require("tape"),
 tape("octree.size() returns the number of points in the octree", function(test) {
   var q = d3_octree.octree();
   test.equal(q.size(), 0);
-  q.add([0, 0]).add([1, 2]);
+  q.add([0, 0, 0]).add([1, 2, 3]);
   test.equal(q.size(), 2);
   test.end();
 });
 
 tape("octree.size() correctly counts coincident nodes", function(test) {
   var q = d3_octree.octree();
-  q.add([0, 0]).add([0, 0]);
+  q.add([0, 0, 0]).add([0, 0, 0]);
   test.equal(q.size(), 2);
   test.end();
 });
