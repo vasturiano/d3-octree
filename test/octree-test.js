efede127d41d409d759ab95ddd6eddd5c31a5cd9
Vasco Asturiano <vasturiano@medallia.com>
2017-03-15 15:14:17 -0700

    Fix octree-test

--- test/octree-test.js
+++ test/octree-test.js
@@ -4,7 +4,7 @@ var tape = require("tape"),
 tape("d3.octree() creates an empty octree", function(test) {
   var q = d3_octree.octree();
   test.ok(q instanceof d3_octree.octree);
-  test.equal(q.visit(function(node, x0, y0, x1, y1) { throw new Error; }), q);
+  test.equal(q.visit(function(node, x0, y0, z0, x1, y1, z1) { throw new Error; }), q);
   test.equal(q.size(), 0);
   test.equal(q.extent(), undefined);
   test.equal(q.root(), undefined);
@@ -13,15 +13,15 @@ tape("d3.octree() creates an empty octree", function(test) {
 });
 
 tape("d3.octree(nodes) is equivalent to d3.octree().addAll(nodes)", function(test) {
-  var q = d3_octree.octree([[0, 0], [1, 1]]);
+  var q = d3_octree.octree([[0, 0, 0], [1, 1, 1]]);
   test.ok(q instanceof d3_octree.octree);
-  test.deepEqual(q.root(), [{data: [0, 0]},,, {data: [1, 1]}]);
+  test.deepEqual(q.root(), [{data: [0, 0, 0]},,,,,,, {data: [1, 1, 1]}]);
   test.end();
 });
 
 tape("d3.octree(nodes, x, y) is equivalent to d3.octree().x(x).y(y).addAll(nodes)", function(test) {
-  var q = d3_octree.octree([{x: 0, y: 0}, {x: 1, y: 1}], function(d) { return d.x; }, function(d) { return d.y; });
+  var q = d3_octree.octree([{x: 0, y: 0, z: 0}, {x: 1, y: 1, z: 1}], function(d) { return d.x; }, function(d) { return d.y; }, function(d) { return d.z; });
   test.ok(q instanceof d3_octree.octree);
-  test.deepEqual(q.root(), [{data: {x: 0, y: 0}},,, {data: {x: 1, y: 1}}]);
+  test.deepEqual(q.root(), [{data: {x: 0, y: 0, z: 0}},,,,,,, {data: {x: 1, y: 1, z: 1}}]);
   test.end();
 });
