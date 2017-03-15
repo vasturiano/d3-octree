130df04393cfc6a660c6c8cc10dfca68514117b9
vasco <vastur@gmail.com>
2017-03-15 01:19:38 -0700

    Migrate remove

--- src/remove.js
+++ src/remove.js
@@ -1,5 +1,5 @@
 export default function(d) {
-  if (isNaN(x = +this._x.call(null, d)) || isNaN(y = +this._y.call(null, d))) return this; // ignore invalid points
+  if (isNaN(x = +this._x.call(null, d)) || isNaN(y = +this._y.call(null, d)) || isNaN(z = +this._z.call(null, d))) return this; // ignore invalid points
 
   var parent,
       node = this._root,
@@ -8,14 +8,19 @@ export default function(d) {
       next,
       x0 = this._x0,
       y0 = this._y0,
+      z0 = this._z0,
       x1 = this._x1,
       y1 = this._y1,
+      z1 = this._z1,
       x,
       y,
+      z,
       xm,
       ym,
+      zm,
       right,
       bottom,
+      deep,
       i,
       j;
 
@@ -27,9 +32,10 @@ export default function(d) {
   if (node.length) while (true) {
     if (right = x >= (xm = (x0 + x1) / 2)) x0 = xm; else x1 = xm;
     if (bottom = y >= (ym = (y0 + y1) / 2)) y0 = ym; else y1 = ym;
-    if (!(parent = node, node = node[i = bottom << 1 | right])) return this;
+    if (deep = z >= (zm = (z0 + z1) / 2)) z0 = zm; else z1 = zm;
+    if (!(parent = node, node = node[i = deep << 2 | bottom << 1 | right])) return this;
     if (!node.length) break;
-    if (parent[(i + 1) & 3] || parent[(i + 2) & 3] || parent[(i + 3) & 3]) retainer = parent, j = i;
+    if (parent[(i + 1) & 7] || parent[(i + 2) & 7] || parent[(i + 3) & 7] || parent[(i + 4) & 7] || parent[(i + 5) & 7] || parent[(i + 6) & 7] || parent[(i + 7) & 7]) retainer = parent, j = i;
   }
 
   // Find the point to remove.
@@ -46,8 +52,8 @@ export default function(d) {
   next ? parent[i] = next : delete parent[i];
 
   // If the parent now contains exactly one leaf, collapse superfluous parents.
-  if ((node = parent[0] || parent[1] || parent[2] || parent[3])
-      && node === (parent[3] || parent[2] || parent[1] || parent[0])
+  if ((node = parent[0] || parent[1] || parent[2] || parent[3] || parent[4] || parent[5] || parent[6] || parent[7])
+      && node === (parent[7] || parent[6] || parent[5] || parent[4] || parent[3] || parent[2] || parent[1] || parent[0])
       && !node.length) {
     if (retainer) retainer[j] = node;
     else this._root = node;
