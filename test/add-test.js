70385052926897e00d909b5f9aa8aa4660e76f8e
Vasco Asturiano <vasturiano@medallia.com>
2017-03-15 19:13:47 -0700

    Partial fix add-test

--- test/add-test.js
+++ test/add-test.js
@@ -3,53 +3,55 @@ var tape = require("tape"),
 
 tape("octree.add(datum) creates a new point and adds it to the octree", function(test) {
   var q = d3_octree.octree();
-  test.deepEqual(q.add([0, 0]).root(), {data: [0, 0]});
-  test.deepEqual(q.add([1, 1]).root(), [{data: [0, 0]},,, {data: [1, 1]}]);
-  test.deepEqual(q.add([1, 0]).root(), [{data: [0, 0]}, {data: [1, 0]},, {data: [1, 1]}]);
-  test.deepEqual(q.add([0, 1]).root(), [{data: [0, 0]}, {data: [1, 0]}, {data: [0, 1]}, {data: [1, 1]}]);
-  test.deepEqual(q.add([0.4, 0.4]).root(), [[{data: [0, 0]},,, {data: [0.4, 0.4]}], {data: [1, 0]}, {data: [0, 1]}, {data: [1, 1]}]);
+  test.deepEqual(q.add([0, 0, 0]).root(), {data: [0, 0, 0]});
+  test.deepEqual(q.add([1, 1, 1]).root(), [{data: [0, 0, 0]},,,,,,, {data: [1, 1, 1]}]);
+  test.deepEqual(q.add([1, 0, 0]).root(), [{data: [0, 0, 0]}, {data: [1, 0, 0]},,,,,, {data: [1, 1, 1]}]);
+  test.deepEqual(q.add([0, 1, 0]).root(), [{data: [0, 0, 0]}, {data: [1, 0, 0]}, {data: [0, 1, 0]},,,,, {data: [1, 1, 1]}]);
+  test.deepEqual(q.add([0, 0, 1]).root(), [{data: [0, 0, 0]}, {data: [1, 0, 0]}, {data: [0, 1, 0]},, {data: [0, 0, 1]},,, {data: [1, 1, 1]}]);
+  test.deepEqual(q.add([0.4, 0.4, 0.4]).root(), [[{data: [0, 0, 0]},,,,,,, {data: [0.4, 0.4, 0.4]}], {data: [1, 0, 0]}, {data: [0, 1, 0]},, {data: [0, 0, 1]},,, {data: [1, 1, 1]}]);
   test.end();
 });
 
 tape("octree.add(datum) handles points being on the perimeter of the octree bounds", function(test) {
-  var results = [], q = d3_octree.octree().extent([[0, 0], [1, 1]]);
-  test.deepEqual(q.add([0, 0]).root(), {data: [0, 0]});
-  test.deepEqual(q.add([1, 1]).root(), [{data: [0, 0]},,, {data: [1, 1]}]);
-  test.deepEqual(q.add([1, 0]).root(), [{data: [0, 0]}, {data: [1, 0]},, {data: [1, 1]}]);
-  test.deepEqual(q.add([0, 1]).root(), [{data: [0, 0]}, {data: [1, 0]}, {data: [0, 1]}, {data: [1, 1]}]);
+  var results = [], q = d3_octree.octree().extent([[0, 0, 0], [1, 1, 1]]);
+  test.deepEqual(q.add([0, 0, 0]).root(), {data: [0, 0, 0]});
+  test.deepEqual(q.add([1, 1, 1]).root(), [{data: [0, 0, 0]},,,,,,, {data: [1, 1, 1]}]);
+  test.deepEqual(q.add([1, 0, 0]).root(), [{data: [0, 0, 0]}, {data: [1, 0, 0]},,,,,, {data: [1, 1, 1]}]);
+  test.deepEqual(q.add([0, 1, 0]).root(), [{data: [0, 0, 0]}, {data: [1, 0, 0]}, {data: [0, 1, 0]},,,,, {data: [1, 1, 1]}]);
+  test.deepEqual(q.add([0, 0, 1]).root(), [{data: [0, 0, 0]}, {data: [1, 0, 0]}, {data: [0, 1, 0]},, {data: [0, 0, 1]},,, {data: [1, 1, 1]}]);
   test.end();
 });
 
 tape("octree.add(datum) handles points being to the top of the octree bounds", function(test) {
-  var results = [], q = d3_octree.octree().extent([[0, 0], [2, 2]]);
-  test.deepEqual(q.add([1, -1]).extent(), [[0, -2], [4, 2]]);
+  var results = [], q = d3_octree.octree().extent([[0, 0, 0], [2, 2, 2]]);
+  test.deepEqual(q.add([1, -1, 1]).extent(), [[0, -2, 0], [4, 2, 4]]);
   test.end();
 });
 
 tape("octree.add(datum) handles points being to the right of the octree bounds", function(test) {
-  var results = [], q = d3_octree.octree().extent([[0, 0], [2, 2]]);
-  test.deepEqual(q.add([3, 1]).extent(), [[0, 0], [4, 4]]);
+  var results = [], q = d3_octree.octree().extent([[0, 0, 0], [2, 2]]);
+  test.deepEqual(q.add([3, 1]).extent(), [[0, 0, 0], [4, 4]]);
   test.end();
 });
 
 tape("octree.add(datum) handles points being to the bottom of the octree bounds", function(test) {
-  var results = [], q = d3_octree.octree().extent([[0, 0], [2, 2]]);
-  test.deepEqual(q.add([1, 3]).extent(), [[0, 0], [4, 4]]);
+  var results = [], q = d3_octree.octree().extent([[0, 0, 0], [2, 2]]);
+  test.deepEqual(q.add([1, 3]).extent(), [[0, 0, 0], [4, 4]]);
   test.end();
 });
 
 tape("octree.add(datum) handles points being to the left of the octree bounds", function(test) {
-  var results = [], q = d3_octree.octree().extent([[0, 0], [2, 2]]);
+  var results = [], q = d3_octree.octree().extent([[0, 0, 0], [2, 2]]);
   test.deepEqual(q.add([-1, 1]).extent(), [[-2, 0], [2, 4]]);
   test.end();
 });
 
 tape("octree.add(datum) handles coincident points by creating a linked list", function(test) {
-  var q = d3_octree.octree().extent([[0, 0], [1, 1]]);
-  test.deepEqual(q.add([0, 0]).root(), {data: [0, 0]});
-  test.deepEqual(q.add([1, 0]).root(), [{data: [0, 0]}, {data: [1, 0]},, ]);
-  test.deepEqual(q.add([0, 1]).root(), [{data: [0, 0]}, {data: [1, 0]}, {data: [0, 1]}, ]);
-  test.deepEqual(q.add([0, 1]).root(), [{data: [0, 0]}, {data: [1, 0]}, {data: [0, 1], next: {data: [0, 1]}}, ]);
+  var q = d3_octree.octree().extent([[0, 0, 0], [1, 1, 1]]);
+  test.deepEqual(q.add([0, 0, 0]).root(), {data: [0, 0, 0]});
+  test.deepEqual(q.add([1, 0]).root(), [{data: [0, 0, 0]}, {data: [1, 0]},, ]);
+  test.deepEqual(q.add([0, 1]).root(), [{data: [0, 0, 0]}, {data: [1, 0]}, {data: [0, 1]}, ]);
+  test.deepEqual(q.add([0, 1]).root(), [{data: [0, 0, 0]}, {data: [1, 0]}, {data: [0, 1], next: {data: [0, 1]}}, ]);
   test.end();
 });
 
